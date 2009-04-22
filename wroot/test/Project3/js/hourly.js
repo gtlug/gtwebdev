eebed5400495ee0ec39036db3daaa1b44965f8a4
NiX0n <BiggoronSword+GitHub@gmail.com>
2009-04-22 17:24:40 +0000

    Removed: Unnecessary code from hourly.js that was copied from index.js

--- wroot/test/Project3/js/hourly.js
+++ wroot/test/Project3/js/hourly.js
@@ -1,43 +1,29 @@
-﻿var Hourly = {
+﻿var Hourly = 
+{
     /**
-    * Page bootstrap
-    * @param Event event
-    * @return void
-    */
-    init: function(event) {
+     * Page bootstrap
+     * @param Event event
+     * @return void
+     */
+    init: function(event) 
+    {
         Hourly.setDataSet(1);
     },
 
     /**
-    * Navigation anchor event handler
-    * NOTE: Event handlers are bound to event objects
-    * The 'this' keyword represents the <a> tag.
-    * @param Event event
-    */
-    navigation_Click: function(event) {
-        Index.setContent(this.href);
-        Index.setNavigationSelected(this.parentNode);
-        // prevent event from bubbling up
-        Event.stop(event);
-    },
-
-    /**
-    * Set content location
-    * @param string href location to set content to 
-    */
-    setContent: function(href) {
-        window.frames[0].location = href;
-    },
-
-    /**
-    * Update navigation to "highlight" selected page
-    * @param HTMLElement selected <li> element
-    */
-    setDataSet: function(col) {
-        $('data').select('td.selected').each(function(td) {
+     * Update navigation to "highlight" selected page
+     * @param integer col
+     */
+    setDataSet: function(col) 
+    {
+        $('data').select('td.selected').each(function(td) 
+        {
             td.removeClassName('selected');
         });
-        $('data').select('td.col' + col).each(function(td) {
+        var select = 'td.col' + col;
+        alert(select);
+        $('data').select(select).each(function(td) 
+        {
             td.addClassName('selected');
         });
     }
