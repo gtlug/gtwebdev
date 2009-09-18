7e41cf7052fd538cdcfeaa3931bff56d52b32600
cblake777 <cblake777@gmail.com>
2009-09-18 06:32:17 +0000

    CHANGEME: attempting to add lib/Gtwebdev

---
+++ lib/Gtwebdev/Util.php
@@ -0,0 +1,54 @@
+<?php
+class Gtwebdev_Util {
+	function formatException(Exception $e)
+	{
+		return 
+			$e->getMessage() . 
+			" in " . $e->getFile() . 
+			" on line " . $e->getLine() . 
+			"\n" . $e->getTraceAsString()
+		;
+	}
+	
+	function addLeftContent($blockTitle, $blockId, $blockContent, $blockPriority=5)
+	{
+		$leftContent = Zend_Registry::get('leftContent');
+		
+		array_push($leftContent,
+			array(
+				'title'=>$blockTitle,
+				'divid'=>$blockId,
+				'content'=>$blockContent,
+				'priority'=>$blockPriority
+		));
+		
+		Zend_Registry::set('leftContent', $leftContent);
+	}
+	
+	public function getLeftContent()
+	{
+		return Gtwebdev_Util::sortPriority(Zend_Registry::get('leftContent'));
+	}
+	
+	function sortPriority($srtArray) {
+		usort($srtArray, array('Gtwebdev_Util','_compare_priority'));
+		return $srtArray;
+	}
+	
+	private function _compare_priority($a, $b)
+	{
+		return strnatcmp($a['priority'], $b['priority']);
+	}
+	
+	function sortArrayByArray($array, $orderArray) {
+		$ordered = array();
+		
+		foreach($orderArray as $key) {
+			if(array_key_exists($key, $array)) {
+				$ordered[$key] = $array[$key];
+				unset($array[$key]);
+			}
+		}
+		return $ordered + $array;
+	}
+}
