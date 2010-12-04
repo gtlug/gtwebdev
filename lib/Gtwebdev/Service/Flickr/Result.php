c7ccdc8533368b30a7fff9d5a238a6d3f2804c4a
NiX0n <BiggoronSword+GitHub@gmail.com>
2010-12-04 17:38:11 +0000

    Added: Serializable properties for overriding and caching purposes

---
+++ lib/Gtwebdev/Service/Flickr/Result.php
@@ -0,0 +1,5 @@
+<?php
+class Gtwebdev_Service_Flickr_Result extends Zend_Service_Flickr_Result
+{
+	// nothing to do yet
+}
--- lib/Gtwebdev/Service/Flickr/ResultSet.php
+++ lib/Gtwebdev/Service/Flickr/ResultSet.php
@@ -5,17 +5,38 @@ class Gtwebdev_Service_Flickr_ResultSet extends Zend_Service_Flickr_ResultSet
 	private $_xml;
 	private $_flickr;
 	
+	private $__results;
+	
 	public function __construct(DOMDocument $dom, Zend_Service_Flickr $flickr)
 	{
 		parent::__construct($dom, $flickr);
 		$this->_dom = $dom;
 		$this->_flickr = $flickr;
+		
+		if(!$this->__results)
+			$this->__results= array();
+		// force results 
+		foreach($this as $n);
 	}
 	
+    /**
+     * Implements SeekableIterator::current()
+     *
+     * @return Zend_Service_Flickr_Result
+     */
+    public function current()
+    {
+    	if(!isset($this->__results[$this->key()]))
+    	{
+    		$this->__results[$this->key()] = new Gtwebdev_Service_Flickr_Result($this->_results->item($this->key()), $this->_flickr);
+    	}
+        return $this->__results[$this->key()];
+    }
+    	
     public function __sleep()
     {
     	$this->_xml = $this->_dom->saveXML();
-        return array('_flickr', '_xml');
+        return array('_flickr', '_xml', '__results');
     }
     
     public function __wakeup()
